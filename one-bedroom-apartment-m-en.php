71150bedf82e87408d534f9204a8e34b7522704f
novljanin <novljanin@gmail.com>
2022-10-23 18:39:45 +0200

    Add files via upload

---
+++ one-bedroom-apartment-m-en.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>One-Bedroom Apartment - M | Official Booking Page - Apartments Pax Herceg Novi</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="https://www.check.deals/direct-booking/core/framework/libs/pj/css/pj.bootstrap.min.css" type="text/css" rel="stylesheet" />
+<link href="https://www.check.deals/direct-booking/index.php?controller=pjFront&action=pjActionLoadCss&cid=2" type="text/css" rel="stylesheet" />
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+/* Style the header */
+.header {
+  background-color: #ffffff;
+  padding: 0px;
+  text-align: center;
+  font-size: 35px;
+}
+
+/* Create three unequal columns that floats next to each other */
+.column {
+  float: left;
+  padding: 10px;
+  height: 100%;
+}
+
+/* Left and right column */
+.column.side {
+  width: 15%;
+}
+
+/* Middle column */
+.column.middle {
+  width: 70%;
+}
+
+/* Clear floats after the columns */
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+
+/* Style the footer */
+.footer {
+  background-color: #f1f1f1;
+  padding: 10px;
+  text-align: center;
+}
+
+/* Responsive layout - makes the three columns stack on top of each other instead of next to each other */
+@media (max-width: 800px) {
+  .column.side, .column.middle {
+    width: 100%;
+  }
+}
+</style>
+</head>
+<body>
+
+
+<style>
+  iframe {
+    width: 1px;
+    min-width: 100%;
+    min-height: 100%;
+  }
+</style>
+<script src="/js/iframeResizer.min.js"></script>
+<iframe id="myIframe" src="https://www.check.deals/direct-booking/property/one-bedroom-apartment-m/index.html" frameborder="0" ></iframe>
+<script>
+  iFrameResize({ log: true }, '#myIframe')
+</script>
+
+<div class="row">
+  <div class="column side" style="background-color:#ffffff;"></div>
+  <div class="column middle" style="background-color:#ffffff;"><script type="text/javascript" src="https://www.check.deals/direct-booking/index.php?controller=pjFront&action=pjActionLoad&cid=2&affiliate=apartmanihercegnovi.com&view=3<?php echo (isset($_GET['date_from'])) ? ('&date_from='.$_GET['date_from']) : '';?><?php echo (isset($_GET['date_to'])) ? ('&date_to='.$_GET['date_to']) : '';?>"></script></div>
+  <div class="column side" style="background-color:#ffffff;"></div>
+</div><br><br>
+
+<div class="footer">
+  <p><b><a href="https://www.apartmanihercegnovi.com/en/">Apartments Pax Herceg Novi Official Booking Page ©</a></b></p>
+</div>
+
+</body>
+</html>
+
+
+
